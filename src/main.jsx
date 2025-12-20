a9928b5111db46f45c8dfbbd96e6259e6fa836e5
Grizzed <k.v.malay@gmail.com>
2025-12-21 00:37:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // Restore original app to reproduce and fix runtime errors
-import App from './App.jsx'
+import App from './App.original.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
